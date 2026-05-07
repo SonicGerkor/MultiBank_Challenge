a2bb1f2fdb8b6e4b5e8d98dd841a78d8c0f2e516
German Battiston <ger.battiston@gmail.com>
2026-05-07 09:53:56 -0300

    First Commit

---
+++ MultibankChallenge/MultibankChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultibankChallengeApp.swift
+//  MultibankChallenge
+//
+//  Created by German Battiston on 07/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct MultibankChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# MultiBank_Challenge
